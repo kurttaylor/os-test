e01877c3c7ee8da4c2732bc4e069bf05b74295a9
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-02-15 11:28:44 +0000

    testing adding new R script

---
+++ analysis/figure.R
@@ -0,0 +1,15 @@
+library('tidyverse')
+
+setwd("/Users/kt17109/OneDrive - University of Bristol/ehr_postdoc/os_test/os-test")
+
+df_input <- read_csv(
+  here::here("output", "input.csv"),
+  col_types = cols(patient_id = col_integer(),age = col_double())
+)
+
+plot_age <- ggplot(data=df_input, aes(df_input$age)) + geom_histogram()
+
+ggsave(
+  plot= plot_age,
+  filename="descriptive.png", path=here::here("output"),
+)
